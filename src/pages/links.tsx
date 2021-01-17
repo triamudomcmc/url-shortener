76a00ce8ca04aad6791a5ae55a64ca63fcb9323d
Peter <peterphongpak@gmail.com>
2021-01-18 03:10:16 +0700

    Fix design

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -16,7 +16,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   return {
     props: {data: item},
-    revalidate: 10
+    revalidate: 60
   }
 
 }
@@ -32,18 +32,34 @@ const links = ({ data }) => {
   useEffect(() => {
     setContent((prev) => {
       let rawData: JSX.Element = <></>
+      let margin = false
       data.forEach((value) => {
-        rawData =
-          <>
-            {rawData}
-            <div className="mt-6 content-panel w-full rounded-2xl md:rounded-3xl hover:bg-pink-50">
-              <Link href={value.link}>
-                <div className="flex flex-col justify-center text-center h-14 md:h-16 my-1">
-                  <h1>{value.title}</h1>
-                </div>
-              </Link>
-            </div>
-          </>
+        if (margin) {
+          rawData =
+            <>
+              {rawData}
+              <div className="mt-6 content-panel w-full rounded-2xl md:rounded-3xl hover:bg-pink-50">
+                <Link href={value.link}>
+                  <div className="flex flex-col justify-center text-center h-14 md:h-16 my-1">
+                    <h1>{value.title}</h1>
+                  </div>
+                </Link>
+              </div>
+            </>
+        }else{
+          rawData =
+            <>
+              {rawData}
+              <div className="content-panel w-full rounded-2xl md:rounded-3xl hover:bg-pink-50">
+                <Link href={value.link}>
+                  <div className="flex flex-col justify-center text-center h-14 md:h-16 my-1">
+                    <h1>{value.title}</h1>
+                  </div>
+                </Link>
+              </div>
+            </>
+          margin = true
+        }
       })
       return rawData
     })
@@ -54,17 +70,21 @@ const links = ({ data }) => {
         <div className="flex mt-14 md:mt-28 w-4/5 h-4/5 custom-min-h max-w-md custom-max-h md:max-h-full">
           <img className="absolute w-72 md:w-auto" src="./vectors/bg.svg"/>
           <div className="flex flex-col item-center round-lg w-full glass-panel rounded-3xl">
-            <div className="mt-12 md:mt-16 mx-6 h-full md:mx-10">
-              <h1 className="text-center text-purple-400 font-extrabold text-3xl md:text-4xl">TUCMC Links</h1>
-              <p className="mt-8 md:mt-12 text-center font-semibold text-xs md:text-base text-gray-500">Discover all the
+            <div className="flex flex-col items-center mt-8 md:mt-12 mx-6 h-full md:mx-10">
+              <img className="w-4/6 custom-max-w" src="./vectors/tableTab.svg"/>
+              <h1 className="mt-3 md:mt-6 text-center text-purple-600 font-extrabold text-2xl md:text-3xl">TUCMC Links</h1>
+              <p className="text-center font-medium text-xs md:text-base text-purple-600">Discover all the
                 links provided by us here :) and feel free to check them out!</p>
-              <div className="w-full overflow-custom h-3/5 md:h-3/5 mt-2 pb-8 md:mt-14 font-semibold text-xs md:text-base font-display text-gray-700">
+              <div className="mt-4 md:mt-6 border-b-2 border-purple-500 w-2/5">
+
+              </div>
+              <div className="w-full overflow-custom h-2/5 md:h-2/4 mt-4 md:pb-10 md:mt-6 font-semibold text-xs md:text-base font-display text-purple-700">
                 {content}
               </div>
             </div>
           </div>
         </div>
-        <img className="mt-10 md:mt-8 mb-8 z-10" src="./vectors/TUCMC.svg"/>
+        <img className="mt-10 md:mt-8 mb-8 z-10 w-44 md:w-72" src="./vectors/TUCMC.svg"/>
       </div>
   )
 }
