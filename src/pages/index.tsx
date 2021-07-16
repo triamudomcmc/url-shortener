b210700afa2ac6d6da833fc058f19f3ffbc64977
pryter <peterphongpak@gmail.com>
2021-07-17 03:16:38 +0700

    Fix title issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
 import InputForm from "./components/InputForm";
 
-const Index = () => (
-  <div className="flex flex-col items-center justify-center min-h-screen">
-    <div className="text-6xl font-bold font-display pb-8">
-      Url Shortener
+const Index = () => {
+
+  useEffect(() => {
+    document.title = "URL Shortener"
+  })
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="text-6xl font-bold font-display pb-8">
+        Url Shortener
+      </div>
+      <InputForm/>
     </div>
-    <InputForm/>
-  </div>
-)
+  )
+}
 
 export default Index
