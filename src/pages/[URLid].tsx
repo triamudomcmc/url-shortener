09fe0421ec02ff9ae482349f94872855eabc263a
pryter <peterphongpak@gmail.com>
2021-07-18 02:06:34 +0700

    Generate 2 hrs static url instead

--- src/pages/[URLid].tsx
+++ src/pages/[URLid].tsx
@@ -1,31 +1,49 @@
-import Router from 'next/router'
+import Router, {useRouter} from 'next/router'
 import React from 'react';
-import {GetServerSideProps} from 'next';
+import {GetServerSideProps, GetStaticPaths, GetStaticProps} from 'next';
 import initialiseDB from '../handlers/firebase-admin'
 
-export const getServerSideProps: GetServerSideProps = async ({ params}) => {
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {paths: [], fallback: true}
+}
+
+export const getStaticProps: GetStaticProps = async ({ params}) => {
+
     const db = initialiseDB.collection("locations").doc(params.URLid.toString())
     const URLbase = await db.get()
     let URLtarget: string
+
     if (URLbase.exists) {
         URLtarget = URLbase.get("target")
     }else{
         URLtarget = "error"
     }
+
     return {
-        props: {target: URLtarget, title: URLbase.get("title") || null}
+        props: {target: URLtarget, title: URLbase.get("title") || null},
+        revalidate: 2 * 60 * 60
     }
 }
 
 const Page = ({ target, title }) => {
+
+    const router = useRouter()
+
     React.useEffect(() => {
-        if (target !== "error") {
-            Router.push(target)
+        if (!router.isFallback) {
+            if (target !== "error") {
+                Router.push(target)
+            }
+            if (title) {
+                document.title = title
+            }
         }
-        if (title) {
-            document.title = title
-        }
-    })
+    }, [router.isFallback])
+
+    if (router.isFallback) {
+        return <div>Loading...</div>
+    }
+
     if (target !== "error") {
         return (<p>{title}</p>)
     }else{
