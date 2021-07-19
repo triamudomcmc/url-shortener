c0358f47aefb0932c626fae6511aeccf636a1e90
pryter <peterphongpak@gmail.com>
2021-07-19 15:12:14 +0700

    Fix safari popup issue

--- src/handlers/auth.tsx
+++ src/handlers/auth.tsx
@@ -118,16 +118,17 @@ function useProvideAuth(token) {
     const jsonResult = await res.json()
     return jsonResult
   }
+
   const signin = () => {
     if (prevPop) {
       prevPop.close()
     }
-    const wid = window.open()
+    const wid = window.open("about:blank","_blank", "width=492,height=740")
     setPrevPop(wid)
 
     genToken().then(jsonResult => {
       if(jsonResult.status) {
-        prevPop.location = `https://account.triamudom.club/auth?authToken=${jsonResult.data.authToken}`
+        wid.location.replace(`https://account.triamudom.club/auth?authToken=${jsonResult.data.authToken}`)
       }
     })
   }
