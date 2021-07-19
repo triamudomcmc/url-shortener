5d92acfa48708b6140f6d19ca62293fe6dd64601
pryter <peterphongpak@gmail.com>
2021-07-19 15:19:03 +0700

    Fix auth token issue

--- src/handlers/auth.tsx
+++ src/handlers/auth.tsx
@@ -128,6 +128,7 @@ function useProvideAuth(token) {
 
     genToken().then(jsonResult => {
       if(jsonResult.status) {
+        setAuthToken(jsonResult.data.authToken)
         wid.location.replace(`https://account.triamudom.club/auth?authToken=${jsonResult.data.authToken}`)
       }
     })
