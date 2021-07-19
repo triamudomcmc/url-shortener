f8a2377577a25d291854027c7d134f9af5eba83e
pryter <peterphongpak@gmail.com>
2021-07-19 15:02:10 +0700

    Fix safari popup issue

--- src/handlers/auth.tsx
+++ src/handlers/auth.tsx
@@ -98,10 +98,7 @@ function useProvideAuth(token) {
     }
   }, [prevPop])
 
-  const signin = async () => {
-    if (prevPop) {
-      prevPop.close()
-    }
+  const genToken = async () => {
     const fp = await FingerprintJS.load()
     const fingerPrint = await fp.get();
 
@@ -119,20 +116,20 @@ function useProvideAuth(token) {
     })
 
     const jsonResult = await res.json()
-
-    console.log(jsonResult)
-
-    if(jsonResult.status) {
-      setAuthToken(jsonResult.data.authToken)
-      const button = document.createElement("button")
-      button.onclick = () => {
-        const wid = window.open(`https://account.triamudom.club/auth?authToken=${jsonResult.data.authToken}`, "Login with TUCMC Account", "width=492,height=740")
-        setPrevPop(wid)
-      }
-      document.body.appendChild(button)
-      button.click()
-      button.remove()
+    return jsonResult
+  }
+  const signin = () => {
+    if (prevPop) {
+      prevPop.close()
     }
+    const wid = window.open()
+    setPrevPop(wid)
+
+    genToken().then(jsonResult => {
+      if(jsonResult.status) {
+        prevPop.location = `https://account.triamudom.club/auth?authToken=${jsonResult.data.authToken}`
+      }
+    })
   }
 
   useEffect(() => {
