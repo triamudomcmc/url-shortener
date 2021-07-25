66361ca55c17c2c0a6165d71e75571f17b53f856
pryter <peterphongpak@gmail.com>
2021-07-25 17:15:54 +0700

    Bring new loading system to links

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -1,39 +1,63 @@
 import Link from "next/link";
 import React, {useEffect, useState} from "react";
 import {GetStaticProps} from "next";
-import initialisedDB from "../handlers/firebase-admin";
-import objectArray from "../types/objectArray";
+import {request} from "../lib/request";
+import {useRouter} from "next/router";
+import hash from "object-hash"
 
-export const getStaticProps: GetStaticProps = async (context) => {
-
-  const db = initialisedDB.collection("portal")
-  const data = await db.listDocuments()
-  const item: objectArray = []
-  for (const doc of data) {
-    let row = await doc.get()
-    item.push({title: row.get("title"),link: row.get("link")})
-  }
-
-  return {
-    props: {data: item},
-    revalidate: 1
-  }
-
-}
-
-const links = ({ data }) => {
+const links = () => {
 
   const [content, setContent] = useState(<></>)
+  const initialPageData = {
+    title: "",
+    description: "",
+    data: [],
+    cache_version: ""
+  }
+
+  const [pageData, setPageData] = useState(initialPageData)
+
+  const fetchPageData = async () => {
+    const id = "tucmc"
+    if (id) {
+
+      // load local cache
+      const localCache = localStorage.getItem(`${id}_cache`)
+      let localCacheVersion = null
+
+      if (localCache) {
+        const parsedLocalCache = JSON.parse(localCache)
+        if ("cache_version" in parsedLocalCache){
+          localCacheVersion = parsedLocalCache.cache_version
+          setPageData(parsedLocalCache)
+          const localCacheHash = hash({...parsedLocalCache, cache_version: ""})
+          if (localCacheHash !== parsedLocalCache.cache_version) {
+            localCacheVersion = null
+            localStorage.setItem(`${id}_cache`, "")
+            setPageData(initialPageData)
+          }
+        }
+      }
+
+      const res = await request("table","fetchPage", {id: id, localCacheVersion: localCacheVersion || ""})
+      if (res.status) {
+        if (res.data.cache_version) {
+          setPageData(res.data)
+          localStorage.setItem(`${id}_cache`, JSON.stringify(res.data))
+        }
+      }
+    }
+  }
 
   useEffect(() => {
-    document.title = "TUCMC Portal"
+    fetchPageData()
   },[])
 
   useEffect(() => {
     setContent((prev) => {
       let rawData: JSX.Element = <></>
       let margin = false
-      data.forEach((value) => {
+      pageData.data.forEach((value) => {
         if (margin) {
           rawData =
             <>
@@ -63,29 +87,27 @@ const links = ({ data }) => {
       })
       return rawData
     })
-  },[data])
+  },[pageData])
 
   return (
     <div className="flex flex-col items-center w-full h-full min-h-screen mid-complex-gradient">
-        <div className="flex mt-14 md:mt-28 w-4/5 h-4/5 custom-min-h max-w-md custom-max-h md:max-h-full">
-          <img className="absolute w-72 md:w-auto" src="./vectors/bg.svg"/>
-          <div className="flex flex-col item-center round-lg w-full glass-panel rounded-3xl">
-            <div className="flex flex-col items-center mt-8 md:mt-12 mx-6 h-full md:mx-10">
-              <img className="w-4/6 custom-max-w" src="./vectors/timeTab.png"/>
-              <h1 className="mt-3 md:mt-6 text-center text-purple-625 font-extrabold text-2xl md:text-3xl">TUCMC Links</h1>
-              <p className="text-center font-medium text-xs md:text-base text-purple-625">Discover all the
-                links provided by us here :) and feel free to check them out!</p>
-              <div className="mt-4 md:mt-6 border-b-2 border-purple-625 w-2/5">
+      <div className="flex mt-14 md:mt-28 w-4/5 h-4/5 custom-min-h max-w-md custom-max-h md:max-h-full">
+        <div className="flex flex-col item-center round-lg w-full glass-panel rounded-3xl">
+          <div className="flex flex-col items-center mt-8 md:mt-12 mx-6 h-full md:mx-10">
+            <img className="w-4/6 custom-max-w" src="/vectors/timeTab.png"/>
+            <h1 className="mt-3 md:mt-6 text-center text-purple-625 font-extrabold text-2xl md:text-3xl">{pageData.title}</h1>
+            <p className="text-center font-medium text-xs md:text-base text-purple-625">{pageData.description}</p>
+            <div className="mt-4 md:mt-6 border-b-2 border-purple-625 w-2/5">
 
-              </div>
-              <div className="px-1 w-full overflow-custom h-2/5 md:h-2/4 mt-4 pb-12 md:mt-6 font-semibold text-xs md:text-base font-display text-purple-625">
-                {content}
-              </div>
+            </div>
+            <div className="px-1 w-full overflow-custom h-2/5 md:h-2/4 mt-4 pb-12 md:mt-6 font-semibold text-xs md:text-base font-display text-purple-625">
+              {content}
             </div>
           </div>
         </div>
-        <img className="mt-10 md:mt-8 mb-8 z-10 w-44 md:w-64" src="./vectors/TUCMC.png"/>
       </div>
+      <img className="mt-10 md:mt-8 mb-8 z-10 w-44 md:w-64" src="/vectors/TUCMC.png"/>
+    </div>
   )
 }
 
