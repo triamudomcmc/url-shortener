8ccd5446c046efa6432a06b45e4d1674e9a712a2
betich <betich365@gmail.com>
2021-08-30 06:13:12 +0700

    :tada: change h1 to p and change some wording

--- src/components/PageLink.tsx
+++ src/components/PageLink.tsx
@@ -2,20 +2,20 @@ import Link from "next/link";
 import { FC } from "react";
 
 interface PageLinkProps {
-	title: string,
-	link: string
+	title: string;
+	link: string;
 }
-  
+
 const PageLink: FC<PageLinkProps> = ({ title, link }) => {
 	return (
-	  <div className="content-panel w-full rounded-2xl md:rounded-3xl hover:bg-pink-50 cursor-pointer">
-		<Link href={link}>
-		  <div className="flex flex-col justify-center text-center h-14 md:h-16 my-1">
-			<h1>{title}</h1>
-		  </div>
-		</Link>
-	  </div>
+		<div className="content-panel w-full rounded-2xl md:rounded-3xl hover:bg-pink-50 cursor-pointer">
+			<Link href={link}>
+				<div className="flex flex-col justify-center text-center h-14 md:h-16 my-1">
+					<p>{title}</p>
+				</div>
+			</Link>
+		</div>
 	);
-}
+};
 
 export default PageLink;
