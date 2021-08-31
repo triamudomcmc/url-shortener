346cc35536bb20396650049f97e00a2fd2c224a5
betich <betich365@gmail.com>
2021-08-31 15:01:58 +0700

    :construction: change router redirect to admin again

--- src/pages/u/admin/index.tsx
+++ src/pages/u/admin/index.tsx
@@ -20,7 +20,7 @@ const Page = () => {
       <h1>Sign-in to edit your page</h1>
       {!userData ? <SigninWithTUCMC/> : <div className="flex space-x-2">
         <button onClick={signout} className="h-10 py-1 px-4 rounded-md bg-white shadow-md border border-gray-700 border-opacity-40">Logout</button>
-        <button onClick={() => {Router.push("/u/edit")}} className="h-10 py-1 px-4 rounded-md bg-white shadow-md border border-gray-700 border-opacity-40">Edit</button>
+        <button onClick={() => {Router.push("/u/admin/edit")}} className="h-10 py-1 px-4 rounded-md bg-white shadow-md border border-gray-700 border-opacity-40">Edit</button>
         <div className="border border-gray-700 border-opacity-40 rounded-md">
           {userData.pages?.map((item, index, arr) => {
             return <h1 key={index} className={classnames("cursor-pointer px-4 py-1", index === 0 ? "rounded-t-md" : index + 1 === arr.length && "rounded-b-md", currentEditing === item && "bg-blue-400 text-white font-medium")} onClick={() => {
