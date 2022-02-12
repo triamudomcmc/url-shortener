2bf837884dcb2e58acf97e6f0c9f8ef7aec2afe9
betich <betich365@gmail.com>
2022-02-12 19:37:53 +0700

    add gtag

--- src/components/PageLink.tsx
+++ src/components/PageLink.tsx
@@ -2,20 +2,30 @@ import Link from "next/link";
 import { FC } from "react";
 
 interface PageLinkProps {
-	title: string;
-	link: string;
+  title: string;
+  link: string;
 }
 
 const PageLink: FC<PageLinkProps> = ({ title, link }) => {
-	return (
-		<div className="content-panel w-full rounded-2xl md:rounded-3xl hover:bg-pink-50 cursor-pointer">
-			<Link href={link}>
-				<div className="flex flex-col justify-center text-center h-14 md:h-16 my-1 px-10">
-					<p>{title}</p>
-				</div>
-			</Link>
-		</div>
-	);
+  return (
+    <div className="content-panel w-full rounded-2xl md:rounded-3xl hover:bg-pink-50 cursor-pointer">
+      <Link href={link}>
+        <div
+          onClick={() => {
+            // @ts-ignore
+            window.gtag("event", "link_click", {
+              event_category: "link_click",
+              event_label: title,
+              link: link,
+            });
+          }}
+          className="flex flex-col justify-center text-center h-14 md:h-16 my-1 px-10"
+        >
+          <p>{title}</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default PageLink;
