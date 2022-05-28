86e6f701b0c130ae649875276cd86f4cc3b9622a
betich <betich365@gmail.com>
2022-05-28 10:59:48 +0700

    naknaknak

--- src/config/firebaseCert.ts
+++ src/config/firebaseCert.ts
@@ -1,21 +1,20 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv";
 
 const firebaseCert = () => {
+  dotenv.config();
 
-    dotenv.config()
+  return {
+    type: process.env.FCERT_TYPE,
+    projectId: process.env.FCERT_PROJECT_ID,
+    private_key_id: process.env.FCERT_PRIVATE_KEY_ID,
+    private_key: process.env.FCERT_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FCERT_CLIENT_EMAIL,
+    client_id: process.env.FCERT_CLIENT_ID,
+    auth_uri: process.env.FCERT_AUTH_URI,
+    token_uri: process.env.FCERT_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FCERT_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FCERT_CLIENT_X509_CERT_URL,
+  };
+};
 
-    return {
-        type: process.env.FCERT_TYPE,
-        projectId: process.env.FCERT_PROJECT_ID,
-        private_key_id: process.env.FCERT_PRIVATE_KEY_ID,
-        private_key: process.env.FCERT_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        client_email: process.env.FCERT_CLIENT_EMAIL,
-        client_id: process.env.FCERT_CLIENT_ID,
-        auth_uri: process.env.FCERT_AUTH_URI,
-        token_uri: process.env.FCERT_TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.FCERT_AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.FCERT_CLIENT_X509_CERT_URL
-    }
-}
-
-export default firebaseCert()
+export default firebaseCert();
