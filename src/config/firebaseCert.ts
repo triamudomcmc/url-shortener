f7cfe369b3394b4888750be2a63b6ed0fee846a0
pryter <peterphongpak@gmail.com>
2022-11-17 12:58:06 +0700

    Revert to doppler

--- src/config/firebaseCert.ts
+++ src/config/firebaseCert.ts
@@ -4,16 +4,11 @@ const firebaseCert = () => {
   dotenv.config();
 
   return {
-    type: process.env.FCERT_TYPE,
     projectId: process.env.FCERT_PROJECT_ID,
     private_key_id: process.env.FCERT_PRIVATE_KEY_ID,
     private_key: process.env.FCERT_PRIVATE_KEY.replace(/\\n/g, "\n"),
     client_email: process.env.FCERT_CLIENT_EMAIL,
     client_id: process.env.FCERT_CLIENT_ID,
-    auth_uri: process.env.FCERT_AUTH_URI,
-    token_uri: process.env.FCERT_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FCERT_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FCERT_CLIENT_X509_CERT_URL,
   };
 };
 
